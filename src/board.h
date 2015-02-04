aa1edb426482e73ef2fe144b78febc4feecf65e0
Adam Dej <dejko.a@gmail.com>
2015-02-04 17:24:41 +0100

    Fix indentation in board.h

--- src/board.h
+++ src/board.h
@@ -6,27 +6,27 @@
 #include <avr/io.h>
 
 // ---- User Interface ----
-#define LED_R_DDR	DDRB
-#define LED_R_PORT	PORTB
-#define LED_R_PIN	0
+#define LED_R_DDR   DDRB
+#define LED_R_PORT  PORTB
+#define LED_R_PIN   0
 
-#define LED_G_DDR	DDRB
-#define LED_G_PORT	PORTB
-#define LED_G_PIN	2
+#define LED_G_DDR   DDRB
+#define LED_G_PORT  PORTB
+#define LED_G_PIN   2
 
-#define LED_B_DDR	DDRA
-#define LED_B_PORT	PORTA
-#define LED_B_PIN	3
+#define LED_B_DDR   DDRA
+#define LED_B_PORT  PORTA
+#define LED_B_PIN   3
 
-#define SPKR_DDR	DDRA
-#define SPKR_PORT	PORTA
-#define SPKR_PIN	0
+#define SPKR_DDR    DDRA
+#define SPKR_PORT   PORTA
+#define SPKR_PIN    0
 
 // ---- Other interfaces ----
 // These are used by the MCU-specific HAL to determine which interface is used in case
 // of multiple interfaces being available
 
-#define RDR_SPI_NUM			0
-#define CTRL_USART_NUM		0
+#define RDR_SPI_NUM         0
+#define CTRL_USART_NUM      0
 
 #endif
